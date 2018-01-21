914ee1887d043876717febe1f464f463ee5b34c9
psergeev77 <p.sergeev77@gmail.com>
2018-01-21 11:54:02 +0400

    initial commit

---
+++ simple-rest-service/src/main/java/su/psergeev77/boot/rest/HelloWorldConfig.java
@@ -0,0 +1,11 @@
+package su.psergeev77.boot.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldConfig.class, args);
+    }
+}
