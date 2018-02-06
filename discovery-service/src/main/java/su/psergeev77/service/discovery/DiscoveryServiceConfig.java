e6004b6ac1a08de8ef77c1cc3bc26104956f67b2
psergeev77 <p.sergeev77@gmail.com>
2018-02-06 23:27:08 +0400

    added some missing files

---
+++ discovery-service/src/main/java/su/psergeev77/service/discovery/DiscoveryServiceConfig.java
@@ -0,0 +1,16 @@
+package su.psergeev77.service.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceConfig.class, args);
+    }
+
+}
